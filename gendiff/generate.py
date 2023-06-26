244f3e1849830c1e6cd8b6ecb6f5f55896a8f1d9
Zak <yes.its.me.zak@gmail.com>
2023-06-26 20:24:10 +0300

    fix file

--- gendiff/generate.py
+++ gendiff/generate.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 
 def generate_gendiff(file1_path, file2_path):
@@ -9,12 +10,15 @@ def generate_gendiff(file1_path, file2_path):
     all_keys = set(list(data1.keys()) + list(data2.keys()))
     for key in sorted(all_keys):
         if key not in data1:
-            result[f'+{key}'] = data2[key]
+            result[f'+ {key}'] = data2[key]
         elif key not in data2:
-            result[f'-{key}'] = data1[key]
+            result[f'- {key}'] = data1[key]
         elif data1[key] != data2[key]:
-            result[f'-{key}'] = data1[key]
-            result[f'+{key}'] = data2[key]
+            result[f'- {key}'] = data1[key]
+            result[f'+ {key}'] = data2[key]
         else:
-            result[f' {key}'] = data1[key]
-    return json.dumps(result, indent=2)
+            result[f'  {key}'] = data1[key]
+    dict = json.dumps(result, indent=2)
+    diff = re.sub(r'[",]', '', dict)
+    return diff
+
