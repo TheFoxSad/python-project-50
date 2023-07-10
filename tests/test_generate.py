2773eee9835412011ba894706c7a1ee81b31cb9a
Zak <yes.its.me.zak@gmail.com>
2023-07-10 22:27:53 +0300

    Обновлено, добавлены абсолютные пути к фалам

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,19 +1,32 @@
 import pytest
+import os
 from gendiff.generate import generate_gendiff
 
 
-file_path1 = 'tests/fixtures/file1.json'
-file_path2 = 'tests/fixtures/file2.json'
-file_path3 = 'tests/fixtures/file3.json'
-r_result1 = 'tests/fixtures/right_answer1.txt'
-r_result2 = 'tests/fixtures/right_answer2.txt'
-r_result3 = 'tests/fixtures/right_answer3.txt'
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file1 = os.path.join(current_dir, 'fixtures/file1.json')
+file2 = os.path.join(current_dir, 'fixtures/file2.json')
+file3 = os.path.join(current_dir, 'fixtures/file3.json')
+file4 = os.path.join(current_dir, 'fixtures/file4.json')
+file_path1 = os.path.join(current_dir, 'fixtures/filepath1.yml')
+file_path2 = os.path.join(current_dir, 'fixtures/filepath2.yml')
+file_path3 = os.path.join(current_dir, 'fixtures/filepath3.yml')
+file_path4 = os.path.join(current_dir, 'fixtures/filepath4.yml')
+r_result1 = os.path.join(current_dir, 'fixtures/right_answer1.txt')
+r_result2 = os.path.join(current_dir, 'fixtures/right_answer2.txt')
+r_result3 = os.path.join(current_dir, 'fixtures/right_answer3.txt')
+r_result4 = os.path.join(current_dir, 'fixtures/right_answer4.txt')
 
 
 @pytest.mark.parametrize('file1, file2, expected', [
+    (file1, file2, r_result1),
+    (file1, file1, r_result2),
+    (file1, file3, r_result3),
+    (file1, file4, r_result4),
     (file_path1, file_path2, r_result1),
     (file_path1, file_path1, r_result2),
-    (file_path1, file_path3, r_result3)
+    (file_path1, file_path3, r_result3),
+    (file_path1, file_path4, r_result4)
 ])
 def test_generate_gendiff(file1, file2, expected):
     result = generate_gendiff(file1, file2)
